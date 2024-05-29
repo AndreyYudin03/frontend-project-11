889433588e01c98dde12f33ed157783a45855b9c
andrey yudin <onetapandrey@gmail.com>
2024-05-29 15:16:35 +0300

    step 1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/AndreyYudin03/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AndreyYudin03/frontend-project-11/actions)
+
+---
+
+[![Actions Status](https://github.com/AndreyYudin03/frontend-project-11/actions/workflows/main.yml/badge.svg)](https://github.com/AndreyYudin03/frontend-project-11/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/a4c0d2b72471a696a891/maintainability)](https://codeclimate.com/github/AndreyYudin03/frontend-project-11/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/a4c0d2b72471a696a891/test_coverage)](https://codeclimate.com/github/AndreyYudin03/frontend-project-11/test_coverage)
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+import path from "path";
+import autoprefixer from "autoprefixer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: "style-loader",
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: "css-loader",
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer],
+              },
+            },
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
